669ffdbed080502c015f60e0e736d22f6069a995
Fabio Zanela <fabio.fmz18@hotmail.com>
2018-06-28 19:41:14 -0300

    Classe domain de Usuario criada e testada

---
+++ src/main/java/com/fabiozanela/patrimonio/domain/Usuario.java
@@ -0,0 +1,93 @@
+package com.fabiozanela.patrimonio.domain;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	private String email;
+	private String telefone;
+	private String cpf;
+	
+	public Usuario() {
+		
+	}
+
+	public Usuario(Integer id, String nome, String email, String telefone, String cpf) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.cpf = cpf;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
--- src/main/java/com/fabiozanela/patrimonio/resources/UsuarioResources.java
+++ src/main/java/com/fabiozanela/patrimonio/resources/UsuarioResources.java
@@ -1,15 +1,29 @@
 package com.fabiozanela.patrimonio.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fabiozanela.patrimonio.domain.Usuario;
 
 @RestController
 @RequestMapping(value="/usuarios")
 public class UsuarioResources {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "rest está funcionando!"; 
+	public List<Usuario> listar() {
+		
+		Usuario usu1 = new Usuario(1, "Fabio Miranda Zanela","fabio.fmz18@gmail.com","(62) 98121-8297", "999.999.999-99");
+		Usuario usu2 = new Usuario(2, "Alziro Zanela", "alziro@hotmail.com","(62) 99665-5528", "999.999.999-99");
+		
+		List<Usuario> lista = new ArrayList<>();
+		
+		lista.add(usu1);
+		lista.add(usu2);
+		
+		return lista; 
 	}
 }
