6929a247bc791afc6b84298511bc2605de42dadb
Fabio Zanela <fabio.fmz18@hotmail.com>
2018-07-31 15:26:10 -0300

    Paginacao com parametros opcionais no endpoint /itens

--- src/main/java/com/fabiozanela/patrimonio/services/ItemService.java
+++ src/main/java/com/fabiozanela/patrimonio/services/ItemService.java
@@ -5,6 +5,9 @@ import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabiozanela.patrimonio.domain.Item;
@@ -73,5 +76,10 @@ public class ItemService {
 		else { // gera letra maiuscula
 			return (char) (rand.nextInt(26) + 65);
 		}
+	}
+	
+	public Page<Item> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 }
